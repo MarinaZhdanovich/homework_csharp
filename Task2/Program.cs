dceef663d6c7d66a32247a2edf31a6272f094d4d
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-10 15:45:41 +0300

    Homework_1

---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Enter a number 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+ {
+    max = number3;
+}
+Console.WriteLine($"-> {max}");
+
+// if позволяет последовательно сравнить все три числа и обновить переменную max
+// else if проверяется только в том случае, если первое условие было ложным. 
---
+++ Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.Write("Enter a number: ");
+// int number = int.Parse(Console.ReadLine()!);
+
+// while (number > 0)
+// {
+//     if (number % 2 == 0)
+//     {
+//         Console.Write($"{number}, ");
+//     }
+//     number--;
+// }
+
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+int count = 0; //счетчик, который отслеживает количество четных чисел, уже выведенных в цикле
+
+while (number > 0)
+{
+    if (number % 2 == 0)
+    {
+        if (count > 0)   //если count больше нуля, мы выводим запятую и пробел перед каждым числом, кроме первого.
+        {
+             Console.Write(", ");
+        }
+        Console.Write(number);
+        count++;
+    }
+    number--;
+}
+
+
+
+
