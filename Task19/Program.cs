106f3b2fd867a501b4cdb304b1f96d35b8c8c478
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-16 23:42:27 +0300

    Homework3

---
+++ Task19/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+int GetInformation(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine()!);
+    return number;
+}
+
+void Palindrome(int number)
+{
+    int temporary = number;  //для выполнения вычислений, чтобы не изменять исходное число number.
+    int reverse = 0;     //для построения перевернутого числа
+
+    while (temporary != 0)  
+    {
+        int remainder = temporary % 10;  // последняя цифра числа 
+        reverse = reverse * 10 + remainder; 
+        temporary = temporary / 10; // удаляем последнюю цифру (или temporary /= 10 деление с присваиванием)
+    }
+
+    if (number == reverse)
+    {
+        Console.WriteLine($"{number} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> нет");
+    }
+}
+
+int result = GetInformation("Enter a five-digit number: ");
+Palindrome(result);
---
+++ Task21/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек 
+и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine()!);
+    return number;
+}
+
+int xA = GetInfo("Введите x для координаты A: ");
+int yA = GetInfo("Введите y для координаты A: ");
+int zA = GetInfo("Введите z для координаты A: ");
+int xB = GetInfo("Введите x для координаты B: ");
+int yB = GetInfo("Введите y для координаты B: ");
+int zB = GetInfo("Введите z для координаты B: ");
+
+double distance(int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    double resx = Math.Pow((xB - xA), 2);
+    double resy = Math.Pow((yB - yA), 2);
+    double resz = Math.Pow((zB - zA), 2);
+    double result = Math.Sqrt(resx + resy + resz);
+    return result;
+}
+
+double final = distance(xA, yA, zA, xB, yB, zB);
+Console.WriteLine($"Расстояние между двумя точками в 3D пространстве -> {Math.Round(final, 2)}");
+
+
+
+    
