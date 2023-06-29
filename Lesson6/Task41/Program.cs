d902cb9e9a8cfa6cad427a6dfd4c9b3ea5b5f23d
MarinaZhdanovich <marina.zhdanovich@gmail.com>
2023-06-29 18:43:07 +0300

    Lesson6

---
+++ Lesson6/Task41/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+-1, -7, 567, 89, 223-> 3
+*/
+
+/* 1)
+string InputNum(string message)
+{
+    Console.Write(message);
+    return (Console.ReadLine()!);
+}
+
+int CountPositiveNumbers(string numbers)
+{
+    int count = 0;
+    {
+        string[] arrayString = numbers.Split(",");  // разделяем строки на подстроки, используя запятую в качестве разделителя
+        int[] arrayNumber = Array.ConvertAll(arrayString, int.Parse); //массив строк преобразуем в массив целых чисел
+
+        foreach (int item in arrayNumber)  // foreach только для чтения
+        {
+            if (item > 0) count++;
+        }
+    }
+    return count;
+}
+
+string myNumbers = InputNum("Enter numbers separated by commas: ");
+int result = CountPositiveNumbers(myNumbers);
+Console.WriteLine($"Count of positive numbers -> {result}");
+*/
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+
+int CountPositiveNumbers(int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int number = InputNum("Enter a number: ");
+        if (number > 0)
+            count++;
+    }
+    return count;
+}
+
+int size = InputNum("Enter a count of numbers: ");
+int result = CountPositiveNumbers(size);
+Console.WriteLine($"Count of positive numbers -> {result}");
---
+++ Lesson6/Task43/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+double InputNum(string message)
+{
+    Console.Write(message);
+    return double.Parse(Console.ReadLine()!);
+}
+
+void IntersectionPoint(double b1, double b2, double k1, double k2)
+{
+    if (k1 == k2 && b1 == b2)
+        Console.WriteLine("Прямые совпадают.");
+    else if (k1 == k2)
+        Console.WriteLine("Прямые параллельные.");
+    else
+    {                                                   
+        double x = (b2 - b1) / (k1 - k2);  
+        double y = k1 * x + b1;           
+        Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
+double numB1 = InputNum("Enter value of b1: ");
+double numB2 = InputNum("Enter value of b2: ");
+double numK1 = InputNum("Enter value of k1: ");
+double numK2 = InputNum("Enter value of k2: ");
+IntersectionPoint(numB1, numB2, numK1, numK2);
